8d78beb66cf520cd90e9bc20b4f54043444a2db8
Cesar Jimenez <cijgr@hotmail.com>
2018-11-21 12:08:23 -0600

    Assignment 8 Bloc-Jams player bar buttons: completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -75,6 +75,18 @@ class Album extends Component {
          const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
          const newIndex = Math.max(0, currentIndex - 1);
          const newSong = this.state.album.songs[newIndex];
+
+         this.setSong(newSong);
+         this.play();
+      }
+
+
+      // handleNextClick method
+      handleNextClick() {
+         const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+         const newIndex = Math.min(currentIndex + 1, this.state.album.songs.length - 1);
+         const newSong = this.state.album.songs[newIndex];
+
          this.setSong(newSong);
          this.play();
       }
@@ -161,6 +173,7 @@ class Album extends Component {
                currentSong={this.state.currentSong} 
                handleSongClick={() => this.handleSongClick(this.state.currentSong)}
                handlePrevClick={() => this.handlePrevClick()}
+               handleNextClick={() => this.handleNextClick()}
             />
          </section>
       );
