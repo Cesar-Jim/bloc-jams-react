c3114f760fd7ec4f67664d96efc3c7a997ab3b71
Cesar Jimenez <cijgr@hotmail.com>
2019-05-11 09:05:53 -0500

    Ready for deployment

---
+++ test/precache-manifest.38f20be01d3c80c9ddfe7eb8d46b087f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1df8c1b9aac2fab35e",
+    "url": "/Bloc-Jams-Site/static/js/runtime~main.af1df8c1.js"
+  },
+  {
+    "revision": "96d0741fb67b9c51406c",
+    "url": "/Bloc-Jams-Site/static/js/main.96d0741f.chunk.js"
+  },
+  {
+    "revision": "2b4542422a7d811fd806",
+    "url": "/Bloc-Jams-Site/static/js/1.2b454242.chunk.js"
+  },
+  {
+    "revision": "96d0741fb67b9c51406c",
+    "url": "/Bloc-Jams-Site/static/css/main.fb7a7371.chunk.css"
+  },
+  {
+    "revision": "8df3b7c877ba704e8b006f8bd560a5f1",
+    "url": "/Bloc-Jams-Site/index.html"
+  }
+];
